c519d789ae684c41573d6d48ba6e3c9bb92df0a4
Stephen Obialor <stevobialor@gmail.com>
2023-06-25 17:08:41 +0000

    handler for stat and status path

---
+++ controllers/AppController.js
@@ -0,0 +1,23 @@
+import dbClient from '../utils/db';
+
+import redisClient from '../utils/redis';
+
+class AppController {
+  static async getStatus(req, res) {
+    const statusOb = {
+      'redis': redisClient.isAlive(),
+      'db': dbClient.isAlive(),
+    };
+    res.send(statusOb);
+  }
+
+  static async getStats(req, res) {
+    const stats = {
+      'users': await dbClient.nbUsers(),
+      'files': await dbClient.nbFiles(),
+    };
+    res.send(stats);
+  }
+}
+
+export default AppController;
