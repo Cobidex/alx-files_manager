9e26960f3e23df9c8abf7afb541d870799ecf540
Stephen Obialor <stevobialor@gmail.com>
2023-06-29 14:33:38 +0000

    correct eslint errors

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,18 +1,12 @@
-import { ObjectId } from 'mongodb';
-
 import { v4 as uuidv4 } from 'uuid';
-
 import crypto from 'crypto';
-
 import dbClient from '../utils/db';
-
 import redisClient from '../utils/redis';
 
 function hash(password) {
   const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
-  return  hashedPassword;
+  return hashedPassword;
 }
-
 
 class AuthController {
   static async getConnect(req, res) {
@@ -23,19 +17,17 @@ class AuthController {
       const decodedCredentials = Buffer.from(encoded, 'base64').toString('utf-8');
       const [email, password] = decodedCredentials.split(':');
 
-      const user = await dbClient.findUser({'email': email, 'password': hash(password)});
+      const user = await dbClient.findUser({ email, password: hash(password) });
 
       if (!user) {
-        res.status(401).send({'Error': 'unauthorized'});
-        return;
+        return res.status(401).send({ Error: 'unauthorized' });
       }
       const token = uuidv4();
       const key = `auth_${token}`;
       await redisClient.set(key, user._id.toString(), 24 * 60);
-      res.status(200).send({'token': token});
-    } else {
-      res.status(401).send({'Error': 'empty authentication header'});
+      return res.status(200).send({ token });
     }
+    return res.status(401).send({ error: 'No authentication header' });
   }
 
   static async getDisconnect(req, res) {
@@ -43,12 +35,10 @@ class AuthController {
     const key = `auth_${token}`;
     const id = await redisClient.get(key);
     if (!id) {
-      res.status(401).send({'Error': 'unauthorized'});
-      return;
+      return res.status(401).send({ Error: 'unauthorized' });
     }
-    const user = await dbClient.findUser({_id: ObjectId(id)});
     await redisClient.del(key);
-    res.status(204).send();
+    return res.status(204).send();
   }
 }
 
